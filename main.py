24fa3a6105f98308876b3afad0a76e3f4bd4bf78
hotman <hotmanww@gmail.com>
2021-08-31 14:36:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import shutil
 from pathlib import Path
 def main(path):
     name=path.split('/')[-1]
-    subprocess.call("./generate.py --test -p {0}".format(name),shell=True)
-    shutil.copytree("{0}/in".format(path),"build/{0}/in".format(path))
-    shutil.copytree("{0}/out".format(path),"build/{0}/out".format(path))
+    subprocess.call("library-checker-problems/generate.py --test -p {0}".format(name),shell=True)
+    shutil.copytree("library-checker-problems/{0}/in".format(path),"build/{0}/in".format(path))
+    shutil.copytree("library-checker-problems/{0}/out".format(path),"build/{0}/out".format(path))
 tomls = list(filter(lambda p: not p.match('test/**/info.toml'),
                         Path('.').glob('**/info.toml')))
 tomls = sorted(tomls, key=lambda x: x.parent.name)
