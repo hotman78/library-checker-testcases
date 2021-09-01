11158cd192a9365cac1aef10477a5bd3d2bd9d7f
hotman78 <hotmanww@gmail.com>
2021-09-01 11:46:18 +0900

    upload graph

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def main():
     tomls = sorted(tomls, key=lambda x: x.parent.name)
     for x in tomls:
         problem=x.parent
-        if problem.parent.name=='datastructure':
+        if problem.parent.name=='graph':
             make_testcase(problem.parent.name,problem.name)
         make_problem_page(problem.parent.name,problem.name)
     make_toppage()
